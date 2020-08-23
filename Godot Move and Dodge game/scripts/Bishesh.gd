8a1312ddbc50bcbbf01b4c041b4d3a8af955fb70
Bisheshwor Neupane <kabichneu@gmail.com>
2020-08-23 20:28:18 +0545

    after adding blender mesh into Godot and setting the rotation and mesh and materials of the objects

--- Godot Move and Dodge game/scripts/Bishesh.gd
+++ Godot Move and Dodge game/scripts/Bishesh.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
-var speed = 5
+var speed = 9
 var velocity = Vector3(0,0,0)
+var rotatspeed = 25
 
 func _ready():
 	pass
@@ -11,17 +12,21 @@ func _physics_process(delta):
 		velocity.x = 0
 	elif Input.is_action_just_pressed("ui_right"):
 		velocity.x = speed
+		$MeshInstance.rotate_z(deg2rad(-rotatspeed))
 	elif Input.is_action_just_pressed("ui_left"):
 		velocity.x = -speed
+		$MeshInstance.rotate_z(deg2rad(rotatspeed))
 	else:
-		velocity.x = lerp(velocity.x, 0 , 0.08)
+		velocity.x = lerp(velocity.x, 0 , 0.1)
 
 	if Input.is_action_just_pressed("ui_up") and Input.is_action_just_pressed("ui_down"):
 		velocity.z = 0
 	elif Input.is_action_just_pressed("ui_up"):
 		velocity.z = -speed
+		$MeshInstance.rotate_x(deg2rad(-rotatspeed))
 	elif Input.is_action_just_pressed("ui_down"):
 		velocity.z = speed
+		$MeshInstance.rotate_x(deg2rad(rotatspeed))
 	else:
 		velocity.z = lerp(velocity.z, 0 , 0.08)
 
